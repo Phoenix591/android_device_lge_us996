408daac632738657a40be092700494ebb156c382
Kyle Elbert <kcelbert@gmail.com>
2019-04-23 06:31:54 -0500

    Compatibility change for potential oreo twrp builds

--- omni_h990.mk
+++ omni_h990.mk
@@ -18,4 +18,8 @@ PRODUCT_NAME := omni_h990
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_COPY_FILES += device/lge/$(PRODUCT_DEVICE)/recovery.fstab:recovery/root/etc/twrp.flags
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 26; echo $$?),0) #if oreo or above
+PRODUCT_COPY_FILES += system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+else
 PRODUCT_COPY_FILES += bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+endif
